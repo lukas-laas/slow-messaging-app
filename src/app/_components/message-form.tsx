50b8ff852aa33a25e164d935bddb83fb98fc21bd
lukas-laas <lukas.s.laas@gmail.com>
2024-07-28 11:40:25 +0200

    basic styling for message input

--- src/app/_components/message-form.tsx
+++ src/app/_components/message-form.tsx
@@ -2,9 +2,23 @@ import { postMessage } from "@/queries";
 
 export default function MessageForm() {
   return (
-    <form action={postMessage} method="POST">
-      <input type="text" placeholder="Message" name="message" />
-      <button type="submit">Send</button>
+    <form
+      action={postMessage}
+      method="POST"
+      className="flex flex-row w-full gap-2 p-2"
+    >
+      <input
+        type="text"
+        placeholder="Message"
+        name="message"
+        className="border border-solid border-neutral-400 w-full rounded-md p-1"
+      />
+      <button
+        type="submit"
+        className="bg-green-400 hover:bg-green-300 rounded-md p-1"
+      >
+        Send
+      </button>
     </form>
   );
 }
