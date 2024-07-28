a3b21aa9f29387648128bf7162de4cd8fa490a74
lukas-laas <lukas.s.laas@gmail.com>
2024-07-28 21:16:21 +0200

    add back message list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import MessageForm from "./_components/message-form";
+import MessageList from "./_components/message-list";
 import NavBar from "./_components/navbar";
 
 export default async function Home() {
   return (
     <main className="flex flex-col max-h-screen h-full max-w-lg mx-auto shadow-md">
       <NavBar />
+      <MessageList />
       <div className="w-full bottom-0">
         <MessageForm />
       </div>
