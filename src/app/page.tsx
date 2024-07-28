5c265e19fe8d4f12e48a3bdbeda9f28deff9780d
lukas-laas <lukas.s.laas@gmail.com>
2024-07-28 12:47:48 +0200

    basic styling for chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,32 +8,36 @@ export default async function Home() {
   const session = await getSession();
 
   return (
-    <main className="flex flex-col">
+    <main className="flex flex-col max-h-screen">
       <h1>Slow-messaging</h1>
       <LogOut />
-      {messages.length ? (
-        messages.map(
-          (message) =>
-            message && (
-              <p
-                key={message.id}
-                className={message.username == session.user ? "self-end" : ""}
-              >
-                <strong>{message.username}: </strong>
-                <br />
-                {message.message
-                  ? message.message
-                  : `Message available at ${new Date(
-                      message.time.getTime() + 3600000
-                    ).toLocaleTimeString()}`}{" "}
-                - {message.time.toLocaleTimeString()}
-              </p>
-            )
-        )
-      ) : (
-        <>No messages</>
-      )}
-      <MessageForm />
+      <div className="h-max overflow-y-auto flex flex-col">
+        {messages.length ? (
+          messages.map(
+            (message) =>
+              message && (
+                <p
+                  key={message.id}
+                  className={message.username == session.user ? "self-end" : ""}
+                >
+                  <strong>{message.username}: </strong>
+                  <br />
+                  {message.message
+                    ? message.message
+                    : `Message available at ${new Date(
+                        message.time.getTime() + 3600000
+                      ).toLocaleTimeString()}`}{" "}
+                  - {message.time.toLocaleTimeString()}
+                </p>
+              )
+          )
+        ) : (
+          <>No messages</>
+        )}
+      </div>
+      <div className="w-full absolute bottom-0">
+        <MessageForm />
+      </div>
     </main>
   );
 }
